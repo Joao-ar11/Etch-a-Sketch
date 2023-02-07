b87032ac78daa6955e259cb4d83462e521b5dc29
joao-ar11 <jarlpereira29@gmail.com>
2023-02-07 15:43:34 -0300

    Add button that change square quantity
    Create createSketchBook() and destroySketchBook()

--- javascript.js
+++ javascript.js
@@ -1,11 +1,27 @@
-let squareQuantity = 16;
-const sketchBook = document.querySelector("#sketchBook");
-for (let i = 0; i < Math.pow(squareQuantity, 2); i++) {
-    const square = document.createElement("div");
-    square.classList.toggle("square")
-    square.style.backgroundColor = "#ffffff";
-    square.style.height = `${960 / squareQuantity}px`;
-    square.style.width = `${960 / squareQuantity}px`;
-    square.addEventListener("mouseover", () => square.style.backgroundColor = "#000000");
-    sketchBook.appendChild(square);
+function createSketchBook(squareQuantity) {
+    const sketchBook = document.querySelector("#sketchBook");
+    for (let i = 0; i < Math.pow(squareQuantity, 2); i++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square")
+        square.style.backgroundColor = "#ffffff";
+        square.style.height = `${960 / squareQuantity}px`;
+        square.style.width = `${960 / squareQuantity}px`;
+        square.addEventListener("mouseover", () => square.style.backgroundColor = "#000000");
+        sketchBook.appendChild(square);
+    }
 }
+
+function destroySketchBook() {
+    const sketchBook = document.querySelector("#sketchBook");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => sketchBook.removeChild(square));
+}
+
+let squareQuantity = 16;
+createSketchBook(squareQuantity);
+const squareQuantityButton = document.querySelector("#squareQuantity");
+squareQuantityButton.addEventListener("click", () => {
+    squareQuantity = +prompt("Type quantity of squares on each line (max 100)");
+    destroySketchBook();
+    createSketchBook(squareQuantity);
+})
